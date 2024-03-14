364e2ef476013acef29011b873591c85bd0936f9
funfun <alpha5179@naver.com>
2024-03-14 23:54:18 +0900

    first commit

---
+++ src/main/java/com/example/sixpack/SixpackApplication.java
@@ -0,0 +1,13 @@
+package com.example.sixpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixpackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sixpack/SixpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sixpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
