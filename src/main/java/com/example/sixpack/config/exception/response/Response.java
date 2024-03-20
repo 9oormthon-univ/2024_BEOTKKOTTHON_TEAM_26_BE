0c6c58cdc3321d1005427b13b76c7e86c3159939
Lwonbin <lwb9036@gmail.com>
2024-03-21 01:07:00 +0900

    chore response 변경중

--- src/main/java/com/example/sixpack/config/exception/response/Response.java
+++ src/main/java/com/example/sixpack/config/exception/response/Response.java
@@ -5,11 +5,11 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@JsonInclude(JsonInclude.Include.NON_NULL) //  null 값을 가지는 필드는, JSON 응답에 포함되지 않음
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class Response {
-    private boolean isSuccess;
+    private boolean success;
     private int code;
     private Result result;
 
